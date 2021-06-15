3899e3301d0922efca105b1cf4cbb895dbd8fded
sebaek <sebaek@gmail.com>
2021-06-15 12:10:04 +0900

    method autowired

--- src/main/java/core/test2/Bag.java
+++ src/main/java/core/test2/Bag.java
@@ -1,9 +1,22 @@
 package core.test2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //@Component("bag")
 @Component
 public class Bag {
 
+	private Book book;
+
+	@Override
+	public String toString() {
+		return "Bag [book=" + book + "]";
+	}
+	
+	@Autowired
+	public void setBook(Book book) {
+		this.book = book;
+	}
+	
 }
