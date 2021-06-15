8956f218d72a56748dfddbbf5ac02218d063233f
sebaek <sebaek@gmail.com>
2021-06-15 11:14:11 +0900

    no lombok

--- src/main/java/core/test1/Chair.java
+++ src/main/java/core/test1/Chair.java
@@ -1,12 +1,19 @@
 package core.test1;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
 public class Chair {
 	private Leg leg;
+
+	public Chair() {
+		
+	}
+	
+	public Chair(Leg leg) {
+		this.leg = leg;
+	}
+
+	@Override
+	public String toString() {
+		return "Chair [leg=" + leg + "]";
+	}
+	
 }
--- src/main/java/core/test1/Desk.java
+++ src/main/java/core/test1/Desk.java
@@ -1,15 +1,20 @@
 package core.test1;
 
-import lombok.Setter;
-import lombok.ToString;
-
-@Setter
-@ToString
 public class Desk {
 	private Leg leg;
 	
 //	public Desk(Leg leg) {
 //		this.leg = leg;
 //	}
+
+	public void setLeg(Leg leg) {
+		this.leg = leg;
+	}
+
+	@Override
+	public String toString() {
+		return "Desk [leg=" + leg + "]";
+	}
+	
 	
 }
