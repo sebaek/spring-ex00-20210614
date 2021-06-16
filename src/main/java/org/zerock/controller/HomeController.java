0d5e5fde4f447c068850fc4d72374722783e8c83
sebaek <sebaek@gmail.com>
2021-06-16 15:53:53 +0900

    home

--- src/main/java/org/zerock/controller/HomeController.java
+++ src/main/java/org/zerock/controller/HomeController.java
@@ -32,6 +32,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info(chef.toString());
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
