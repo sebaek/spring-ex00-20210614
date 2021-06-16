30fcbacba94c3a8ca5355e3bcf37c42388b176cd
sebaek <sebaek@gmail.com>
2021-06-16 10:22:51 +0900

    test methods

---
+++ src/test/java/sample/junit/MyTest1.java
@@ -0,0 +1,59 @@
+package sample.junit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class MyTest1 {
+
+	@Test
+	public void test() {
+		// fail("Not yet implemented");
+		int i = 3;
+		if (i != 3) {
+			fail("i는 3이 아닙니다.");
+		}
+	}
+	
+	@Test
+	public void equals() {
+		int i = 3;
+		boolean b = true;
+		Object c = null;
+		
+		assertEquals(i, 3);
+		assertEquals(b, true);
+		assertTrue(b);
+		assertNull(c);
+	}
+	
+	@Test
+	public void notEquals() {
+		int i = 5;
+		boolean b = false;
+		Object c = new Object();
+		
+		assertNotEquals(i, 3);
+		assertFalse(b);
+		assertNotNull(c);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
