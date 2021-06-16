6e0722a7e8a2f4135438bf92c95edd97c95ea0cb
sebaek <sebaek@gmail.com>
2021-06-16 09:29:35 +0900

    sample hotel

--- src/test/java/org/zerock/sample/HotelTests.java
+++ src/test/java/org/zerock/sample/HotelTests.java
@@ -4,9 +4,11 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -14,6 +16,7 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class HotelTests {
 
+	@Setter(onMethod_ = @Autowired)
 	private SampleHotel hotel;
 	
 	@Test
