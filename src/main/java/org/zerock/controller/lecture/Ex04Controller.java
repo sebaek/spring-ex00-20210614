9343b83d863de18c3a4e832d2b0f366ec206cb73
sebaek <sebaek@gmail.com>
2021-06-17 15:21:41 +0900

    customeditor

--- src/main/java/org/zerock/controller/lecture/Ex04Controller.java
+++ src/main/java/org/zerock/controller/lecture/Ex04Controller.java
@@ -17,6 +17,7 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class Ex04Controller {
 
+	//http://localhost:8080/controller/ex04/sub01?date=2020-01-01
 	@RequestMapping("/sub01")
 	public void method01(@RequestParam String date) {
 		log.info("ex04, sub01 method");
@@ -24,6 +25,7 @@ public class Ex04Controller {
 		log.info(date);
 	}
 	
+	// http://localhost:8080/controller/ex04/sub02?date=2020-01-01
 	@RequestMapping("/sub02")
 	public void method02(@RequestParam Date date) {
 		log.info("ex04, sub02 method");
