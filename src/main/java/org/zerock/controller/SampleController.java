36eb4116dbaa9df49025070d2ac0b3ae7f8eb62a
sebaek <sebaek@gmail.com>
2021-06-17 15:47:11 +0900

    customeditor

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,14 +1,20 @@
 package org.zerock.controller;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
+import org.zerock.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -75,7 +81,27 @@ public class SampleController {
 		
 		return "ex01";
 	}
+	
+	// 137쪽 ex03
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo:" + todo);
+		return "ex03";
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+	}
+	
 }
+
+
+
+
+
+
 
 
 
---
+++ src/main/java/org/zerock/controller/lecture/Ex05Controller.java
@@ -0,0 +1,28 @@
+package org.zerock.controller.lecture;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/ex05/*")
+public class Ex05Controller {
+	
+	@RequestMapping("/sub01")
+	public void method01(@DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+		log.info("ex05, sub01 method");
+		log.info(date);
+	}
+}
+
+
+
+
+
+
+
