9b7ed2f0f38ed30b075eaa9c6d81c49420bd6781
sebaek <sebaek@gmail.com>
2021-06-17 14:31:20 +0900

    bean

--- src/main/java/org/zerock/controller/lecture/Ex03Controller.java
+++ src/main/java/org/zerock/controller/lecture/Ex03Controller.java
@@ -2,6 +2,7 @@ package org.zerock.controller.lecture;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.controller.lecture.domain.User;
 
 import lombok.extern.log4j.Log4j;
 
@@ -11,7 +12,35 @@ import lombok.extern.log4j.Log4j;
 public class Ex03Controller {
 	
 	@RequestMapping("/sub01")
-	public void method01() {
+	public void method01(User user) {
+		
+		/*
+		String id = request.getParameter("id");
+		String ageStr = request.getParameter("age");
+		int age = Integer.parseInt(ageStr);
+		
+		User user = new User();
+		user.setId(id);
+		user.setAge(age);
+		*/
+		
 		log.info("ex03, sub01 method");
+		log.info(user);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/org/zerock/controller/lecture/domain/User.java
@@ -0,0 +1,10 @@
+package org.zerock.controller.lecture.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+	private String id;
+	private int age;
+}
