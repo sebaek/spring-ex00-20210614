40c55ec33ce7e5a4db2a17d8a1c8db9bd0d8baea
sebaek <sebaek@gmail.com>
2021-06-17 10:19:08 +0900

    header

--- src/main/java/org/zerock/controller/lecture/Ex01Controller.java
+++ src/main/java/org/zerock/controller/lecture/Ex01Controller.java
@@ -2,6 +2,7 @@ package org.zerock.controller.lecture;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.extern.log4j.Log4j;
 
@@ -15,4 +16,24 @@ public class Ex01Controller {
 //		System.out.println("ex01, sub01 method");
 		log.info("ex01, sub01 method");
 	}
+	
+	@RequestMapping("/sub02")
+	public void method02() {
+		log.info("ex01, sub02 method");
+	}
+	
+	@RequestMapping(value = "/sub03", method = RequestMethod.GET)
+	public void method03() {
+		log.info("ex01, sub03 get method");
+	}
 }
+
+
+
+
+
+
+
+
+
+
