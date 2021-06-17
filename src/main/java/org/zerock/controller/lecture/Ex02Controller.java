b9e2c284f91922ca26a9867902e4a55cbeebe62c
sebaek <sebaek@gmail.com>
2021-06-17 11:46:13 +0900

    handler method argument

--- src/main/java/org/zerock/controller/lecture/Ex02Controller.java
+++ src/main/java/org/zerock/controller/lecture/Ex02Controller.java
@@ -1,5 +1,15 @@
 package org.zerock.controller.lecture;
 
-public class Ex02Controller {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex02/*")
+public class Ex02Controller {	
+	
+	@RequestMapping("/sub01")
+	public void method01() {
+		
+	}
 	
 }
