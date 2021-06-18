16aea4063008cf009509894bdc3462faba6468e6
sebaek <sebaek@gmail.com>
2021-06-18 12:16:37 +0900

    model

---
+++ src/main/java/org/zerock/controller/lecture/Ex08Controller.java
@@ -0,0 +1,47 @@
+package org.zerock.controller.lecture;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.controller.lecture.domain.User;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/ex08/*")
+@Log4j
+public class Ex08Controller {
+	
+	@RequestMapping("/sub01")
+	public void method01(Model model) {
+		log.info("ex08, sub01 method");
+		
+		model.addAttribute("name", "donald");
+		model.addAttribute("age", 22);
+	}
+	
+	@RequestMapping("/sub02")
+	public void method02(Model model) {
+		log.info("ex08, sub02 method");
+		
+		User user = new User();
+		user.setId("bts");
+		user.setAge(22);
+		
+		model.addAttribute("user", user);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/webapp/WEB-INF/views/ex08/sub01.jsp
@@ -0,0 +1,27 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<%@ include file="/WEB-INF/subModules/bootstrapHeader.jsp" %>
+
+<title>Insert title here</title>
+</head>
+<body>
+<div class="container">
+	<h1>ex08 sub01 view</h1>
+	<h3>name : ${name }</h3>
+	<h3>age : ${age }</h3>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
