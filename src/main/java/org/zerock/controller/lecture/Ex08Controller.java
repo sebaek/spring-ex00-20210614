d52c6ac4e7fc264275e2ea727298fedaa661eebf
sebaek <sebaek@gmail.com>
2021-06-18 14:19:05 +0900

    model attribute

--- src/main/java/org/zerock/controller/lecture/Ex08Controller.java
+++ src/main/java/org/zerock/controller/lecture/Ex08Controller.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.controller.lecture.domain.User;
 
 import lombok.extern.log4j.Log4j;
@@ -80,6 +81,28 @@ public class Ex08Controller {
 		model.addAttribute("hello world");
 		return "ex08/sub02";
 	}
+	
+	// http://localhost:8080/controller/ex08/sub08?id=pari&age=2000
+	@RequestMapping("/sub08")
+	public String method08(User user) {
+		
+		log.info("ex08, sub08 method");
+		
+		return "ex08/sub02";
+	}
+	
+	
+	// http://localhost:8080/controller/ex08/sub09?age=999&name=korea
+	@RequestMapping("/sub09")
+	public void method09(@ModelAttribute("age") int age,
+			@ModelAttribute("name") String name) {
+		log.info("ex08, sub09 method");
+		
+		log.info(age);
+		log.info(name);
+		
+	}
+	
 
 }
 
