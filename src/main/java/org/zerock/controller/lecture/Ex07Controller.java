d75c173dba61c4a128859cc69832c9a2ab6dbba3
sebaek <sebaek@gmail.com>
2021-06-18 11:13:29 +0900

    jackson bind

--- src/main/java/org/zerock/controller/lecture/Ex07Controller.java
+++ src/main/java/org/zerock/controller/lecture/Ex07Controller.java
@@ -3,6 +3,7 @@ package org.zerock.controller.lecture;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.zerock.controller.lecture.domain.User;
 
 import lombok.extern.log4j.Log4j;
 
@@ -18,7 +19,37 @@ public class Ex07Controller {
 		return "my response message!!!!!!!";
 		
 	}
+	
+	
+	@RequestMapping("/sub02")
+	public @ResponseBody String method02() {
+		log.info("ex07, sub02 method");
+		
+		User user = new User();
+		user.setId("donald");
+		user.setAge(99);
+		
+		return user.toString();
+	}
+	
+	@RequestMapping("/sub03")
+	public @ResponseBody String method03() {
+		log.info("ex07, sub03 method");
+		
+		User user = new User();
+		user.setId("donald");
+		user.setAge(99);
+		
+		return "{\"id\":" + "\"" + user.getId() + "\", " + "\"age\":" + user.getAge() + "}";
+	}
 }
+
+
+
+
+
+
+
 
 
 
