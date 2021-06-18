7e3a9a48e7e1423d8e11dbbe7fd05def4f456234
sebaek <sebaek@gmail.com>
2021-06-18 12:34:06 +0900

    model attribute

--- src/main/java/org/zerock/controller/lecture/Ex08Controller.java
+++ src/main/java/org/zerock/controller/lecture/Ex08Controller.java
@@ -2,6 +2,7 @@ package org.zerock.controller.lecture;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.controller.lecture.domain.User;
 
@@ -11,48 +12,67 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/ex08/*")
 @Log4j
 public class Ex08Controller {
-	
+
 	@RequestMapping("/sub01")
 	public void method01(Model model) {
 		log.info("ex08, sub01 method");
-		
+
 		model.addAttribute("name", "donald");
 		model.addAttribute("age", 22);
 	}
-	
+
 	@RequestMapping("/sub02")
 	public void method02(Model model) {
 		log.info("ex08, sub02 method");
-		
+
 		User user = new User();
 		user.setId("bts");
 		user.setAge(22);
-		
+
 		model.addAttribute("user", user);
 	}
-	
-	
+
 	@RequestMapping("/sub03")
 	public String method03(Model model) {
 		log.info("ex08, sub03 method");
-		
+
 		User user = new User();
 		user.setId("korea");
 		user.setAge(5000);
-		
+
 //		model.addAttribute("user", user);
 		model.addAttribute(user);
-		
+
 		return "ex08/sub02";
 	}
+
+	// http://localhost:8080/controller/ex08/sub04?id=seoul&age=2000
+	@RequestMapping("/sub04")
+	public String method04(User user, Model model) {
+		log.info("ex08, sub04 method");
+
+		model.addAttribute("user", user);
+
+		return "ex08/sub02";
+	}
+
+	// http://localhost:8080/controller/ex08/sub05?id=jeju&age=9000
+	@RequestMapping("/sub05")
+	public String method05(@ModelAttribute("user") User user, Model model) {
+		log.info("ex08, sub05 method");
+
+		return "ex08/sub02";
+	}
+
+	// http://localhost:8080/controller/ex08/sub06?id=busan&age=8000
+	@RequestMapping("/sub06")
+	public String method06(@ModelAttribute User user, Model model) {
+		log.info("ex08, sub06 method");
+
+		return "ex08/sub02";
+	}
+
 }
-
-
-
-
-
-
-
 
 
 
