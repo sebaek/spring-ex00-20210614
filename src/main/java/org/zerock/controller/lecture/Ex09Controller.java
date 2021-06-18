47ccb7545b57277b46ce504f21f9d2d93f36671f
sebaek <sebaek@gmail.com>
2021-06-18 15:20:02 +0900

    redirect attribute

--- src/main/java/org/zerock/controller/lecture/Ex09Controller.java
+++ src/main/java/org/zerock/controller/lecture/Ex09Controller.java
@@ -1,7 +1,10 @@
 package org.zerock.controller.lecture;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.controller.lecture.domain.User;
 
 import lombok.extern.log4j.Log4j;
 
@@ -12,8 +15,11 @@ public class Ex09Controller {
 
 	// http://localhost:8080/controller/ex09/sub01
 	@RequestMapping("/sub01")
-	public String method01() {
+	public String method01(RedirectAttributes rttr) {
 		log.info("ex09, sub01 method");
+		
+		rttr.addAttribute("name", "jin");
+		rttr.addAttribute("id", 300);
 		
 		return ("redirect:sub02"); // 상대경로
 //		return ("redirect:/ex09/sub02"); // 절대경로
@@ -25,7 +31,40 @@ public class Ex09Controller {
 		
 
 	}
+	
+	@RequestMapping("/sub03")
+	public String method03(RedirectAttributes rttr) {
+		log.info("ex09, sub03 method");
+		
+//		rttr.addAttribute("user", new User()); // xxxxx
+		
+		User user = new User();
+		user.setAge(333333);
+		user.setId("seoul");
+		rttr.addFlashAttribute("user", user);
+		
+		return "redirect:sub04";
+	}
+	
+	@RequestMapping("/sub04")
+	public void method04(Model model) {
+		log.info("ex09, sub04 method");
+		
+		log.info(model.asMap().get("user"));
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
