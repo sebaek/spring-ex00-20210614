3b3985be967868af453b53d6819b4c0a2dbe9fae
sebaek <sebaek@gmail.com>
2021-06-21 10:08:13 +0900

    jdbc url

--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -1,12 +1,16 @@
 package org.zerock.mapper;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -22,6 +26,10 @@ public class BoardMapperTests {
 	@Test
 	public void testGetList() {
 		assertNotNull(mapper);
+		
+		List<BoardVO> list = mapper.getList();
+		
+		assertEquals(5, list.size());
 	}
 
 }
