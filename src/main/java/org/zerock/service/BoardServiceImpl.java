46eabe75a75c92f450031d9e7c362ad8cd784abc
sebaek <sebaek@gmail.com>
2021-06-22 10:28:28 +0900

    service register

--- src/main/java/org/zerock/service/BoardServiceImpl.java
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -22,31 +22,26 @@ public class BoardServiceImpl implements BoardService {
 	
 	@Override
 	public void register(BoardVO board) {
-		// TODO Auto-generated method stub
-		
+		mapper.insertSelectKey(board);
 	}
 
 	@Override
 	public BoardVO get(Long bno) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean modify(BoardVO board) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean remove(Long bno) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public List<BoardVO> getList() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	
--- src/test/java/org/zerock/service/BoardServiceTests.java
+++ src/test/java/org/zerock/service/BoardServiceTests.java
@@ -1,5 +1,6 @@
 package org.zerock.service;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -7,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -24,7 +26,26 @@ public class BoardServiceTests {
 	public void testExist() {
 		assertNotNull(service);
 	}
+	
+	@Test
+	public void testRegister() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글2");
+		board.setContent("새로 작성하는 내용2");
+		board.setWriter("newbie");
+		
+		service.register(board);
+		
+		assertNotEquals(0, board.getBno());
+		
+	}
 }
+
+
+
+
+
+
 
 
 
