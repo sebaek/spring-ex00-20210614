24c6615486ab6598c7a8933a4febb3687db5c224
sebaek <sebaek@gmail.com>
2021-06-22 10:19:07 +0900

    service

---
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,53 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class BoardServiceImpl implements BoardService {
+
+	private BoardMapper mapper;
+	
+//	@Autowired
+//	public BoardServiceImpl(BoardMapper mapper) {
+//		this.mapper = mapper;
+//	}
+	
+	
+	@Override
+	public void register(BoardVO board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
