295a8b9cc66ac884766b4af04ba7669a416c4640
sebaek <sebaek@gmail.com>
2021-06-24 09:22:28 +0900

    sql limit

--- src/main/webapp/WEB-INF/sql/SQL02.sql
+++ src/main/webapp/WEB-INF/sql/SQL02.sql
@@ -1,1 +1,23 @@
 USE spr1;
+
+SELECT * FROM tbl_board
+ORDER BY bno DESC
+LIMIT 5; -- 상위 5 개
+
+SELECT * FROM tbl_board
+ORDER BY bno DESC
+LIMIT 0, 5; -- 0번부터 상위 5 개 (1page)
+
+SELECT * FROM tbl_board
+ORDER BY bno DESC
+LIMIT 5, 5; -- 5번부터 상위 5 개 (2page)
+
+SELECT * FROM tbl_board
+ORDER BY bno DESC
+LIMIT 10, 5; -- 10번부터 상위 5 개 (3page)
+
+
+SELECT * FROM tbl_board
+ORDER BY bno DESC
+LIMIT 5 * (n-1) , 5; -- (n page)
+
