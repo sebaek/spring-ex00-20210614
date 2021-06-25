3999a1003dce03ea51a1917e92de7be1285617ab
sebaek <sebaek@gmail.com>
2021-06-25 09:18:24 +0900

    sql

---
+++ src/main/webapp/WEB-INF/sql/SQL03.sql
@@ -0,0 +1,30 @@
+USE spr1;
+-- 전체 게시물
+SELECT * FROM tbl_board
+ORDER BY bno DESC;
+
+-- 제목 검색
+SELECT * FROM tbl_board
+WHERE title LIKE '%자바%'
+ORDER BY bno DESC
+LIMIT 0, 10;
+
+-- 제목 본문 검색
+SELECT * FROM tbl_board
+WHERE 
+title LIKE '%자바%'
+OR content LIKE '%자바%'
+ORDER BY bno DESC;
+
+-- 제목 본문 작성자 검색
+SELECT * FROM tbl_board
+WHERE 
+title LIKE '%자바%'
+OR content LIKE '%자바%'
+OR writer LIKE '%자바%'
+ORDER BY bno DESC
+LIMIT 0, 10;
+
+
+
+
