3b816f57e1ca4b897846fc96d490ec862d41afd2
sebaek <sebaek@gmail.com>
2021-06-25 14:23:40 +0900

    sql

---
+++ src/test/java/org/zerock/sample/Test11.java
@@ -0,0 +1,24 @@
+package org.zerock.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class Test11 {
+
+	@Test
+	public void test() {
+		List<String> list = new ArrayList<>();
+		list.add("user2");
+		list.add("user3");
+		list.add("user4");
+		
+		test222(list);
+	}
+	
+	public void test222(List<String> types) {
+		
+	}
+
+}
