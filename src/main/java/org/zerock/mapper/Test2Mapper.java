fbb1026532357f25fe446f33cf7fc2cef7065282
sebaek <sebaek@gmail.com>
2021-06-25 09:43:07 +0900

    mapper2

--- src/main/java/org/zerock/mapper/Test2Mapper.java
+++ src/main/java/org/zerock/mapper/Test2Mapper.java
@@ -2,4 +2,5 @@ package org.zerock.mapper;
 
 public interface Test2Mapper {
 
+	public int sql1(String type);
 }
---
+++ src/test/java/org/zerock/mapper/Test2MapperTests.java
@@ -0,0 +1,30 @@
+package org.zerock.mapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class Test2MapperTests {
+
+	private Test2Mapper mapper;
+	
+	@Autowired
+	public void setMapper(Test2Mapper mapper) {
+		this.mapper = mapper;
+	}
+	
+	@Test
+	public void test() {
+		assertNotNull(mapper);
+	}
+
+}
