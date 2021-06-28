762f260ad94e37b1850fdf2ae73d6d5019bb0f8a
sebaek <sebaek@gmail.com>
2021-06-28 09:41:58 +0900

    json

--- src/main/java/org/zerock/controller/lecture/rest/Ex02RestController.java
+++ src/main/java/org/zerock/controller/lecture/rest/Ex02RestController.java
@@ -1,7 +1,11 @@
 package org.zerock.controller.lecture.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.controller.lecture.domain.User;
 
 //@Controller
 //@ResponseBody // 모든 메소드에 붙인 것과 같음
@@ -13,4 +17,59 @@ public class Ex02RestController {
 	public String method01() {
 		return "response body1";
 	}
+	
+	@RequestMapping("/sub02")
+	public User method02() {
+		User user = new User();
+		user.setId("korea");
+		user.setAge(1);
+		
+		return user;
+	}
+	
+	@RequestMapping("/sub03")
+	public List<String> method03() {
+		List<String> list = new ArrayList<>();
+		list.add("java");
+		list.add("script");
+		list.add("object");
+		list.add("notation");
+		
+		return list;
+		
+//		return new String[] {"java", "script", "object", "notation"};
+	}
+	
+	@RequestMapping("/sub04")
+	public List<User> method04() {
+		List<User> list = new ArrayList<>();
+		User user = new User();
+		user.setId("seoul");
+		user.setAge(3);
+		
+		list.add(user);
+		list.add(user);
+		list.add(user);
+		
+		
+		return list;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
