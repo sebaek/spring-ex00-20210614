395c001b5ebb6119bf27f1749fffda6548c40a03
sebaek <sebaek@gmail.com>
2021-06-28 11:12:25 +0900

    responseentity

--- src/main/java/org/zerock/controller/lecture/rest/Ex03RestController.java
+++ src/main/java/org/zerock/controller/lecture/rest/Ex03RestController.java
@@ -1,6 +1,8 @@
 package org.zerock.controller.lecture.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +24,23 @@ public class Ex03RestController {
 		
 		return vo;
 	}
+	
+	@GetMapping("/sub02")
+	public ResponseEntity<BoardVO> method02(Long bno) {
+		BoardVO vo = service.get(bno);
+		
+		if (vo == null) {
+			// 값이 없을 때
+			return new ResponseEntity<BoardVO>(HttpStatus.NOT_FOUND);
+		} else {
+			// 값이 있을 때 OK
+			return new ResponseEntity<BoardVO>(vo, HttpStatus.OK);
+		}
+		
+	}
 }
+
+
 
 
 
