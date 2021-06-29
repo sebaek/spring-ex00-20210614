ba5a2e20af9f95fdf9d60e47f968c36a8dc7694c
sebaek <sebaek@gmail.com>
2021-06-29 15:10:12 +0900

    create reply table

---
+++ src/main/webapp/WEB-INF/sql/SQL04.sql
@@ -0,0 +1,14 @@
+USE spr1;
+
+DESC tbl_board;
+
+CREATE TABLE tbl_reply (
+	rno INT PRIMARY KEY AUTO_INCREMENT,
+    bno INT NOT NULL,
+    reply VARCHAR(512) NOT NULL,
+    replyer VARCHAR(50) NOT NULL,
+    replyDate TIMESTAMP DEFAULT now(),
+    updateDate TIMESTAMP DEFAULT now(),
+    FOREIGN KEY (bno) REFERENCES tbl_board(bno)
+);
+    
