166ad0c4715c2f2ac74711e23ae0f040e4a4de93
sebaek <sebaek@gmail.com>
2021-06-29 10:34:06 +0900

    ajax

--- src/main/java/org/zerock/controller/lecture/rest/Ex09RestController.java
+++ src/main/java/org/zerock/controller/lecture/rest/Ex09RestController.java
@@ -1,5 +1,28 @@
 package org.zerock.controller.lecture.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.controller.lecture.domain.User;
+
+import lombok.extern.log4j.Log4j;
+
+@RestController
+@RequestMapping("/rest09")
+@Log4j
 public class Ex09RestController {
 
+	@RequestMapping("/sub01")
+	public User method01(String id, int age) {
+		User user = new User();
+		user.setId(id);
+		user.setAge(age);
+		
+		return user;
+	}
 }
+
+
+
+
+
+
