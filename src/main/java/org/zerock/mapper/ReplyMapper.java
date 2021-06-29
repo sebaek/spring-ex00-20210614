1d8ca647e44859b681d981d2a9e4dcf034541882
sebaek <sebaek@gmail.com>
2021-06-29 15:24:23 +0900

    mapper

---
+++ src/main/java/org/zerock/mapper/ReplyMapper.java
@@ -0,0 +1,8 @@
+package org.zerock.mapper;
+
+import org.zerock.domain.ReplyVO;
+
+public interface ReplyMapper {
+
+	public int insert(ReplyVO vo);
+}
---
+++ src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -0,0 +1,35 @@
+package org.zerock.mapper;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class ReplyMapperTests {
+
+	@Setter(onMethod_ = @Autowired)
+	private ReplyMapper mapper;
+	
+	@Test
+	public void testMapper() {
+		assertNotNull(mapper);
+	}
+
+}
+
+
+
+
+
+
+
