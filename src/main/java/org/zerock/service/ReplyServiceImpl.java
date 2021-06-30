6d14e4dc7dcb3a1aebb4872de01100677126e180
sebaek <sebaek@gmail.com>
2021-06-30 09:36:09 +0900

    service

---
+++ src/main/java/org/zerock/service/ReplyServiceImpl.java
@@ -0,0 +1,63 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zerock.domain.ReplyVO;
+import org.zerock.mapper.ReplyMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+public class ReplyServiceImpl implements ReplyService {
+	
+	@Setter(onMethod_ = @Autowired)
+	private ReplyMapper mapper;
+	
+	/*
+	@Autowired
+	public void setMapper(ReplyMapper mapper) {
+		this.mapper = mapper;
+	}
+	*/
+
+	@Override
+	public int register(ReplyVO vo) {
+
+		return mapper.insert(vo);
+	}
+
+	@Override
+	public ReplyVO get(Long rno) {
+
+		return mapper.read(rno);
+	}
+
+	@Override
+	public int modify(ReplyVO vo) {
+		// TODO Auto-generated method stub
+		return mapper.update(vo);
+	}
+
+	@Override
+	public int remove(Long rno) {
+		// TODO Auto-generated method stub
+		return mapper.delete(rno);
+	}
+
+	@Override
+	public List<ReplyVO> getList(Long bno) {
+		// TODO Auto-generated method stub
+		return mapper.getList(bno);
+	}
+	
+}
+
+
+
+
+
+
