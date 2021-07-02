14218ac94dd64c4eff9fd156b609b7ad7d87223b
sebaek <sebaek@gmail.com>
2021-07-02 10:13:16 +0900

    pom, root-context

--- src/main/java/org/zerock/service/TxTest1Service.java
+++ src/main/java/org/zerock/service/TxTest1Service.java
@@ -2,6 +2,7 @@ package org.zerock.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mapper.TxTest1Mapper;
 
 import lombok.Setter;
@@ -12,6 +13,7 @@ public class TxTest1Service {
 	@Setter (onMethod_ = @Autowired)
 	private TxTest1Mapper mapper;
 	
+	@Transactional
 	public void insert(String name) {
 		mapper.insert(name);
 		mapper.insert(name + name);
