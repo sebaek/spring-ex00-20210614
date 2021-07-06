8583f2571c68fc6617ad94d7e8fd38f016eaa53a
sebaek <sebaek@gmail.com>
2021-07-06 14:15:11 +0900

    sql8

---
+++ src/main/webapp/WEB-INF/sql/SQL08.sql
@@ -0,0 +1,17 @@
+USE spr1;
+
+CREATE TABLE tbl_member (
+	userid VARCHAR(50) PRIMARY KEY,
+    userpw VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    regdate TIMESTAMP DEFAULT NOW(),
+    updateDate TIMESTAMP DEFAULT NOW(),
+    enabled TINYINT(1) DEFAULT 1
+);
+
+CREATE TABLE tbl_member_auth (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	userid VARCHAR(50) NOT NULL,
+    auth VARCHAR(50) NOT NULL,
+    FOREIGN KEY (userid) REFERENCES tbl_member(userid)
+);
