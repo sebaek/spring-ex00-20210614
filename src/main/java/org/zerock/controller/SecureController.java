50b20c19c45ecad6c206b73c7b30bde511957a10
sebaek <sebaek@gmail.com>
2021-07-06 11:12:36 +0900

    utf-8

--- src/main/java/org/zerock/controller/SecureController.java
+++ src/main/java/org/zerock/controller/SecureController.java
@@ -12,26 +12,19 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SecureController {
 
-	@GetMapping("/all")
+	@GetMapping(value = "/all", produces = "text/plain;charset=utf-8")
 	@ResponseBody
 	public String doAll() {
-		
+
 		log.info("secure all method");
 		return "모두 접근 가능 경로";
 	}
-	
-	
-	@GetMapping("/member")
+
+	@GetMapping(value = "/member", produces = "text/plain;charset=utf-8")
 	@ResponseBody
 	public String doMember() {
 		log.info("secure member method");
-		
+
 		return "멤버만 접근 가능 경로";
 	}
 }
-
-
-
-
-
-
