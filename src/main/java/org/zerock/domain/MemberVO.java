2d5673a690b38c6592f75f5836fc46e52b39eb94
sebaek <sebaek@gmail.com>
2021-07-06 14:26:38 +0900

    vo, mapper

---
+++ src/main/java/org/zerock/domain/MemberVO.java
@@ -0,0 +1,18 @@
+package org.zerock.domain;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	private String userid;
+	private String userpw;
+	private String username;
+	private Date regdate;
+	private Date updateDate;
+	private boolean enabled;
+	
+	private List<AuthVO> authList;
+}
--- src/main/java/org/zerock/mapper/MemberMapper.java
+++ src/main/java/org/zerock/mapper/MemberMapper.java
@@ -1,5 +1,11 @@
 package org.zerock.mapper;
 
+import org.zerock.domain.AuthVO;
+import org.zerock.domain.MemberVO;
+
 public interface MemberMapper {
 
+	public int insert(MemberVO vo);
+	
+	public int insertAuth(AuthVO vo);
 }
